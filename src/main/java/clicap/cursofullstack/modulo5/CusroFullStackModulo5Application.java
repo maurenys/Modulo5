c580a92e17fd0292681fcd776d1c598fc5e71edf
maurenys <maurenys.v@gmail.com>
2022-03-31 21:49:32 -0300

    primero

---
+++ src/main/java/clicap/cursofullstack/modulo5/CusroFullStackModulo5Application.java
@@ -0,0 +1,13 @@
+package clicap.cursofullstack.modulo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusroFullStackModulo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusroFullStackModulo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/clicap/cursofullstack/modulo5/CusroFullStackModulo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package clicap.cursofullstack.modulo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CusroFullStackModulo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
